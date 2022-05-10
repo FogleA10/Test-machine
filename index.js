36f14ce7e50350c4a71ee337d0fd1442bae764fe
audreyfogle <FogleA10@gmail.com>
2022-05-09 22:41:59 -0400

    working on the functions in the index.js

--- index.js
+++ index.js
@@ -8,31 +8,88 @@ const Manager = require("./lib/Manager")
 
 //If you have any questions, contact me by email at [${data.email}](mailto:${data.email}) or on github at [${data.github}](https://github.com/${data.github}).
 
-var employeeArray = [
+var employeeArray = []
+const managerQuestions = [
+    {
+        type:"input",
+        name: "title",
+        message:"What is the manager's name?"
+    },
+    {
+        type:"input",
+        name: "title",
+        message: "What is the Manager's Id?"
+    },
     {
         type:"input",
         name:"title",
-        message:""
-
+        message: "What is the Manager's office number?"
     }
+    //id, email, office number 
+    //what other team mates would you like to add in its own function, keeps asking the same question
+    //different quesitons array for the engineer, Intern,finish manager. 
+]
+function managerPrompt(){
+    inquirer.prompt(managerQuestions)
+    .then((data) =>{
+        var manager = new Manager(data.name, data.id, data.email, data.officeNumber)
+        employeeArray.push(manager)
+    })    
+}
+managerQuestions()
 
+
+function choicesPrompt(){
+    inquirer.prompt(choicesQuestions)
+    .then((data)=>{
+        var choice = new choice (data.name, data.id, data.email)
+        employeeArray.push(choicesQuestions)
+    })
+}
+choicesQuestions()
+const choicesQuestions =[
+    { type:"",
+    name:"",
+    message:"?"
+
+},
 ]
 
-
-.then((data) =>{
-    var manager = new Manager(data.name, data.id, data.email, data.officeNumber)
-    employeeArray.push(manager)
-})
+function internPrompt(){
+inquirer.prompt(internQuestions)
 .then((data) => {
-    var intern = new Intern(data.name, data.id, data.email, data.school)
-    employeeArray.push(intern)
+    var intern = new intern(data.name, data.id, data.email, data.school)
+    employeeArray.push(internQuestions)
 })
+}
+internQuestions()
+const interQuestions = [
+    {
+        type:"",
+        name:"",
+        message:"?"
+    },
+]
+
+function engineerPrompt(){
+    inquirer.prompt(engineerQuestions)
 .then((data) =>{
     var engineer = new Engineer(data.name, data.id, data.email, data.github)
-    employeeArray.push(engineer)
+    employeeArray.push(engineerQuestions)
 })
+}
+engineerQuestions()
+const engineerQuestions = [
+    {type:"",
+    name:"",
+    message:""
+
+    }
+]
+
 .then((data)=> {
     var employee = new Employee( data.name, data.id, data.email)
     employeeArray.push(employee)
 })
+
 //inter, engineer
